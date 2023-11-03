9c19d590f4d221866dbfe9134d737c73292c19d1
khuongyolo <khuongyolo@gmail.com>
2023-11-03 23:34:16 +0900

    touchpad scroll updated 2

--- MapJS.js
+++ MapJS.js
@@ -1,13 +1,13 @@
-window.onload = function() {
+window.onload = () => {
   const list = document.getElementById('list-category');
   let isDragging = false;
   let touchStartX;
   let touchScrollLeft;
 
-  list.addEventListener('wheel', function(event) {
+  list.addEventListener('wheel', (event) => {
     event.preventDefault();
-
-    const scrollSpeed = 10; 
+    
+    const scrollSpeed = 10;
     if (event.deltaY > 0) {
       list.scrollLeft += scrollSpeed;
     } else {
@@ -15,24 +15,22 @@ window.onload = function() {
     }
   });
 
-  list.addEventListener('touchstart', function(event) {
+  list.addEventListener('touchstart', (event) => {
     isDragging = true;
     touchStartX = event.touches[0].clientX;
     touchScrollLeft = list.scrollLeft;
   });
 
-  list.addEventListener('touchend', function() {
+  list.addEventListener('touchend', () => {
     isDragging = false;
   });
 
-  list.addEventListener('touchmove', function(event) {
+  list.addEventListener('touchmove', (event) => {
     if (!isDragging) return;
     event.preventDefault();
-
+    
     const touchCurrentX = event.touches[0].clientX;
-    const scrollAmount = (touchStartX - touchCurrentX) * 0.5; 
+    const scrollAmount = (touchStartX - touchCurrentX) * 2; 
     list.scrollLeft = touchScrollLeft + scrollAmount;
   });
 };
-
-
