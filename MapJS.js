41b824ecf0fbda578ff1683f981d89a73a60e8a0
NamVuonggg <NamVuong453@gmail.com>
2023-11-03 15:38:58 +0900

    updated 3/11/2023

--- MapJS.js
+++ MapJS.js
@@ -1,187 +1,187 @@
-let map;
-let marker;
-let circle;
-let searchInput;
-let zoomInterval;
-const zoomStart = 10;
-const zoomEnd = 12;
-const zoomIncrement = 3;
+// let map;
+// let marker;
+// let circle;
+// let searchInput;
+// let zoomInterval;
+// const zoomStart = 10;
+// const zoomEnd = 12;
+// const zoomIncrement = 3;
 
-var markerPositions = [];
+// var markerPositions = [];
 
-function generateRandomLandCoordinates() {
+// function generateRandomLandCoordinates() {
   // Phạm vi đất liền của ba tỉnh này:
-  const tokyoRange = {
-    minLat: 35.2, 
-    maxLat: 35.9, 
-    minLng: 138.8, 
-    maxLng: 140.8,
-  };
-  const saitamaRange = {
-    minLat: 35.6,
-    maxLat: 36.3, 
-    minLng: 138.9, 
-    maxLng: 139.9,
-  };
-  const kanagawaRange = {
-    minLat: 35.1, 
-    maxLat: 35.7, 
-    minLng: 139.1, 
-    maxLng: 139.9, 
-  };
+  // const tokyoRange = {
+  //   minLat: 35.2, 
+  //   maxLat: 35.9, 
+  //   minLng: 138.8, 
+  //   maxLng: 140.8,
+  // };
+  // const saitamaRange = {
+  //   minLat: 35.6,
+  //   maxLat: 36.3, 
+  //   minLng: 138.9, 
+  //   maxLng: 139.9,
+  // };
+  // const kanagawaRange = {
+  //   minLat: 35.1, 
+  //   maxLat: 35.7, 
+  //   minLng: 139.1, 
+  //   maxLng: 139.9, 
+  // };
 
   // Chọn ngẫu nhiên một trong ba tỉnh
-  const randomProvince = Math.floor(Math.random() * 3);
+//   const randomProvince = Math.floor(Math.random() * 3);
 
-  let selectedRange;
+//   let selectedRange;
 
-  if (randomProvince === 0) {
-    selectedRange = tokyoRange;
-  } else if (randomProvince === 1) {
-    selectedRange = saitamaRange;
-  } else {
-    selectedRange = kanagawaRange;
-  }
+//   if (randomProvince === 0) {
+//     selectedRange = tokyoRange;
+//   } else if (randomProvince === 1) {
+//     selectedRange = saitamaRange;
+//   } else {
+//     selectedRange = kanagawaRange;
+//   }
 
-  const lat = Math.random() * (selectedRange.maxLat - selectedRange.minLat) + selectedRange.minLat;
-  const lng = Math.random() * (selectedRange.maxLng - selectedRange.minLng) + selectedRange.minLng;
+//   const lat = Math.random() * (selectedRange.maxLat - selectedRange.minLat) + selectedRange.minLat;
+//   const lng = Math.random() * (selectedRange.maxLng - selectedRange.minLng) + selectedRange.minLng;
 
-  return { lat, lng };
-}
+//   return { lat, lng };
+// }
 
 // Thêm 100 điểm ngẫu nhiên trên đất liền của Tokyo, Saitama và Kanagawa vào mảng markerPositions
-for (let i = 0; i < 40; i++) {
-  const randomCoordinate = generateRandomLandCoordinates();
-  markerPositions.push(randomCoordinate);
-}
+// for (let i = 0; i < 40; i++) {
+//   const randomCoordinate = generateRandomLandCoordinates();
+//   markerPositions.push(randomCoordinate);
+// }
 
 
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: zoomStart,
-    center: { lat: 35.6894, lng: 139.692 },
-    mapTypeControl: false,
-    mapId: "e670dc860b95d0c5",
-  });
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: zoomStart,
+//     center: { lat: 35.6894, lng: 139.692 },
+//     mapTypeControl: false,
+//     mapId: "e670dc860b95d0c5",
+//   });
 
-  searchInput = document.getElementById("search-input");
+  // searchInput = document.getElementById("search-input");
   // const searchButton = document.getElementById("search-button");
-  const clearButton = document.getElementById("clear-button");
+  // const clearButton = document.getElementById("clear-button");
 
   // searchButton.addEventListener("click", searchLocation);
-  clearButton.addEventListener("click", clear);
+  // clearButton.addEventListener("click", clear);
 
   // Enter search
-  searchInput.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-      searchLocation();
-    }
-  });
+//   searchInput.addEventListener("keyup", function (event) {
+//     if (event.key === "Enter") {
+//       searchLocation();
+//     }
+//   });
 
-  var customIcon = {
-    url: 'pin.png',
-    scaledSize: new google.maps.Size(60, 60),
-  };
+//   var customIcon = {
+//     url: 'pin.png',
+//     scaledSize: new google.maps.Size(60, 60),
+//   };
 
-  for (var i = 0; i < markerPositions.length; i++) {
-    var marker = new google.maps.Marker({
-      position: markerPositions[i],
-      map: map,
-      icon: customIcon,
-      title: "Marker " + (i + 1),
-    });
-  }
+//   for (var i = 0; i < markerPositions.length; i++) {
+//     var marker = new google.maps.Marker({
+//       position: markerPositions[i],
+//       map: map,
+//       icon: customIcon,
+//       title: "Marker " + (i + 1),
+//     });
+//   }
 
-  clear();
-}
+//   clear();
+// }
 
-function clear() {
-  if (marker) {
-    marker.setMap(null);
-  }
-  if (circle) {
-    circle.setMap(null);
-  }
-}
+// function clear() {
+//   if (marker) {
+//     marker.setMap(null);
+//   }
+//   if (circle) {
+//     circle.setMap(null);
+//   }
+// }
 
-function searchLocation() {
-  clear();
-  const location = searchInput.value;
+// function searchLocation() {
+//   clear();
+//   const location = searchInput.value;
 
-  if (!location) {
-    alert("Please enter a location.");
-    return;
-  }
+//   if (!location) {
+//     alert("Please enter a location.");
+//     return;
+//   }
 
-  const geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ address: location }, function (results, status) {
-    if (status === google.maps.GeocoderStatus.OK && results[0]) {
-      const locationLatLng = results[0].geometry.location;
+//   const geocoder = new google.maps.Geocoder();
+//   geocoder.geocode({ address: location }, function (results, status) {
+//     if (status === google.maps.GeocoderStatus.OK && results[0]) {
+//       const locationLatLng = results[0].geometry.location;
 
-      clearInterval(zoomInterval);
+//       clearInterval(zoomInterval);
 
-      map.setZoom(zoomStart);
+//       map.setZoom(zoomStart);
 
-      $("html, body").animate(
-        {
-          scrollTop: $("#map").offset().top,
-        },
-        1000
-      );
+//       $("html, body").animate(
+//         {
+//           scrollTop: $("#map").offset().top,
+//         },
+//         1000
+//       );
 
-      if (!marker) {
-        marker = new google.maps.Marker({
-          position: locationLatLng,
-          map: map,
-        });
-      } else {
-        marker.setPosition(locationLatLng);
-        marker.setMap(map);
-      }
+//       if (!marker) {
+//         marker = new google.maps.Marker({
+//           position: locationLatLng,
+//           map: map,
+//         });
+//       } else {
+//         marker.setPosition(locationLatLng);
+//         marker.setMap(map);
+//       }
 
-      if (!circle) {
-        circle = new google.maps.Circle({
-          strokeColor: "#fff",
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: "#8FBC8F",
-          fillOpacity: 0.1,
-          map: map,
-          center: locationLatLng,
-          radius: 3000,
-        });
-      } else {
-        circle.setCenter(locationLatLng);
-        circle.setMap(map);
-      }
+//       if (!circle) {
+//         circle = new google.maps.Circle({
+//           strokeColor: "#fff",
+//           strokeOpacity: 0.8,
+//           strokeWeight: 2,
+//           fillColor: "#8FBC8F",
+//           fillOpacity: 0.1,
+//           map: map,
+//           center: locationLatLng,
+//           radius: 3000,
+//         });
+//       } else {
+//         circle.setCenter(locationLatLng);
+//         circle.setMap(map);
+//       }
 
-      map.panTo(locationLatLng);
+//       map.panTo(locationLatLng);
 
-      zoomInterval = setInterval(function () {
-        if (map.getZoom() < zoomEnd) {
-          map.setZoom(map.getZoom() + zoomIncrement);
-        } else {
-          clearInterval(zoomInterval);
-        }
-      }, 500);
-    } else {
-      alert("Geocoding was not successful for the following reason: " + status);
-    }
-  });
-}
+//       zoomInterval = setInterval(function () {
+//         if (map.getZoom() < zoomEnd) {
+//           map.setZoom(map.getZoom() + zoomIncrement);
+//         } else {
+//           clearInterval(zoomInterval);
+//         }
+//       }, 500);
+//     } else {
+//       alert("Geocoding was not successful for the following reason: " + status);
+//     }
+//   });
+// }
 
-window.initMap = initMap;
+// window.initMap = initMap;
 
 
-function changetoLightMode(){
-  var styleSheetLink = document.getElementById("styleSheetLink");
-  styleSheetLink.href = "MapGOROlightmode.css";
-}
-function changetoDarkMode(){
-  var styleSheetLink = document.getElementById("styleSheetLink");
-  styleSheetLink.href = "Mapstyle.css";
-}
+// function changetoLightMode(){
+//   var styleSheetLink = document.getElementById("styleSheetLink");
+//   styleSheetLink.href = "MapGOROlightmode.css";
+// }
+// function changetoDarkMode(){
+//   var styleSheetLink = document.getElementById("styleSheetLink");
+//   styleSheetLink.href = "Mapstyle.css";
+// }
 
 // // kéo thả
 // window.onload = function() {
