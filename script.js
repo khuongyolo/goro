b909bab6088b1382725c2d8993a8014a84330628
NamVuonggg <NamVuong453@gmail.com>
2023-11-21 07:32:54 +0900

    updated 21/11/2023

---
+++ script.js
@@ -0,0 +1,32 @@
+function EnglishChange(){
+    document.getElementById("signin-heading").innerHTML = "Sign-In";
+    document.getElementById("signin-label").innerHTML = "User account";
+    document.getElementById("password-label").innerHTML = "User password";
+    document.getElementById("signin-btn").innerHTML = "Confirm";
+    document.getElementById("signin-link-or").innerHTML = "Or";
+    document.getElementById("sign-up").innerHTML = "You don't have account ?";
+    document.getElementById("signup-link").innerHTML = " Sign Up";
+    document.getElementById("forgot-password").innerHTML = " Forgot Password ?";
+}
+
+function VietnameseChange(){
+    document.getElementById("signin-heading").innerHTML = "Đăng nhập";
+    document.getElementById("signin-label").innerHTML = "Tài Khoản";
+    document.getElementById("password-label").innerHTML = "Mật Khẩu";
+    document.getElementById("signin-btn").innerHTML = "Xác Nhận";
+    document.getElementById("signin-link-or").innerHTML = "Hoặc";
+    document.getElementById("sign-up").innerHTML = "Bạn chưa có tài khoản ?";
+    document.getElementById("signup-link").innerHTML = " Đăng kí";
+    document.getElementById("forgot-password").innerHTML = "Quên mật khẩu ?";
+}
+
+function JapaneseChange(){
+    document.getElementById("signin-heading").innerHTML = "サイン";
+    document.getElementById("signin-label").innerHTML = "アカウント";
+    document.getElementById("password-label").innerHTML = "パスワード";
+    document.getElementById("signin-btn").innerHTML = "確認";
+    document.getElementById("signin-link-or").innerHTML = "または";
+    document.getElementById("sign-up").innerHTML = "アカウントまだありいません?";
+    document.getElementById("signup-link").innerHTML = " 登録";
+    document.getElementById("forgot-password").innerHTML = "パスワードを忘れた方?";
+}
