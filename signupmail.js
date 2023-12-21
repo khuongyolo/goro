09afe64e93a817d4d65e6e5cb847624615569188
NamVuonggg <NamVuong453@.com>
2023-12-21 09:26:34 +0900

    updated 21/12/2023

---
+++ signupmail.js
@@ -0,0 +1,34 @@
+function EnglishChange(){
+    
+    document.getElementById("logo-text").innerHTML = "Click the logo to see more about us";
+    document.getElementById("signup-mail-heading").innerHTML = "Sign-Up with Email";
+    document.getElementById("signup-mail-label").innerHTML = "Mail Address";
+    document.getElementById("password-mail-label").innerHTML = "Password";
+    document.getElementById("re-password-mail-label").innerHTML = "Re-enter Password";
+    document.getElementById("term-accept-error").innerHTML = "Please confirm terms of use";
+    document.getElementById("term-accept").innerHTML = "I accepted all the terms of use";    
+    document.getElementById("signup-mail-btn").innerHTML = "Confirm";
+}
+
+function VietnameseChange(){
+    document.getElementById("logo-text").innerHTML = "Click để xem thêm về chúng tôi";
+    document.getElementById("signup-mail-heading").innerHTML = "Đăng kí bằng Email";
+    document.getElementById("signup-mail-label").innerHTML = "Địa chỉ email";
+    document.getElementById("password-mail-label").innerHTML = "Mật khẩu";
+    document.getElementById("re-password-mail-label").innerHTML = "Nhập lại mật khẩu";
+    document.getElementById("term-accept-error").innerHTML = "Bạn hãy xác nhận điều khoản sử dụng";
+    document.getElementById("term-accept").innerHTML = "Tôi đồng ý với các điều khoản sử dụng";    
+    document.getElementById("signup-mail-btn").innerHTML = "Xác Nhận";
+}
+
+function JapaneseChange(){
+    document.getElementById("logo-text").innerHTML = "私たちについて知りたければ、ロゴをクリック";
+    document.getElementById("signup-mail-heading").innerHTML = "Emailで登録";
+    document.getElementById("signup-mail-label").innerHTML = "Emailアドレス";
+    document.getElementById("password-mail-label").innerHTML = "パウワード";
+    document.getElementById("re-password-mail-label").innerHTML = "パスワード再入";
+    document.getElementById("term-accept-error").innerHTML = "利用規約をご確認ください";
+    document.getElementById("term-accept").innerHTML = "すべての利用規約に同意しました";    
+    document.getElementById("signup-mail-btn").innerHTML = "確認";
+
+}
