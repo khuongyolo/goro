7091e00e388a129405700f4df96451c7788dcc12
NamVuonggg <NamVuong453@gmail.com>
2023-12-28 18:51:46 +0900

    updated 28/12/2023

---
+++ forgotpassword.js
@@ -0,0 +1,41 @@
+function EnglishChange(){
+    
+    document.getElementById("logo-text").innerHTML = "Click the logo to see more about us";
+    document.getElementById("tab-title").innerHTML = " GORO | Re-signup";
+    document.getElementById("forgot-password-heading").innerHTML = " Reset Password";
+    document.getElementById("forgot-password-mail-label").innerHTML = "Mail Address";
+    document.getElementById("forgot-password-password-label").innerHTML = "New Password";
+    document.getElementById("forgot-password-re-password-label").innerHTML = "Re-enter Password";
+    document.getElementById("term-accept-error").innerHTML = "Please read and accept terms of service";
+    document.getElementById("term-forgot-password-accept-span1").innerHTML = "I have read and agreed with";
+    document.getElementById("term-forgot-password-accept-span2").innerHTML = " terms of use";
+    document.getElementById("forgot-password-btn").innerHTML = "Confirm";   
+}
+
+function VietnameseChange(){
+    document.getElementById("logo-text").innerHTML = "Click để xem thêm về chúng tôi";
+    document.getElementById("tab-title").innerHTML = " GORO | Đăng kí lại ";
+    document.getElementById("forgot-password-heading").innerHTML = " Đặt lại mật khẩu";
+    document.getElementById("forgot-password-mail-label").innerHTML = "Địa chỉ mail";
+    document.getElementById("forgot-password-password-label").innerHTML = "Mật khẩu mới";
+    document.getElementById("forgot-password-re-password-label").innerHTML = "Nhập lại mật khẩu";
+    document.getElementById("term-accept-error").innerHTML = "Bạn hãy xác nhận điều khoản sử dụng";
+    document.getElementById("term-forgot-password-accept-span1").innerHTML = "Tôi đồng ý với";
+    document.getElementById("term-forgot-password-accept-span2").innerHTML = " các điều khoản sử dụng";
+    document.getElementById("forgot-password-btn").innerHTML = "Xác Nhận";   
+    
+}
+
+function JapaneseChange(){
+    document.getElementById("logo-text").innerHTML = "私たちについて知りたければ、ロゴをクリック";
+    document.getElementById("tab-title").innerHTML = " GORO | 再登録";
+    document.getElementById("forgot-password-heading").innerHTML = " パスワードを再登録";
+    document.getElementById("forgot-password-mail-label").innerHTML = "メールアドレス";
+    document.getElementById("forgot-password-password-label").innerHTML = "新パスワード";
+    document.getElementById("forgot-password-re-password-label").innerHTML = "再入力";
+    document.getElementById("term-accept-error").innerHTML = "利用規約を確認してください";
+    document.getElementById("term-forgot-password-accept-span1").innerHTML = "確認する";
+    document.getElementById("term-forgot-password-accept-span2").innerHTML = "利用規約";
+    document.getElementById("forgot-password-btn").innerHTML = "確認";   
+    
+}
