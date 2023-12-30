8aa07c332d6c3e11aa3bb1ed9571428dc7d1eb5d
NamVuonggg <NamVuong453@.com>
2023-12-31 08:43:54 +0900

    updated 31/12/2023

--- homescreen.js
+++ homescreen.js
@@ -7,6 +7,8 @@ function DisplayUserOption() {
 
 function HideUserOption() {
     var userMenu = document.getElementById("user-menu-container");
+    var screenWidth = window.innerWidth;
+
     
     // Thêm event listener để ẩn phần tử sau khi animation hoàn thành
     userMenu.addEventListener('animationend', function() {
@@ -17,8 +19,19 @@ function HideUserOption() {
         document.querySelector('body').style.overflow = '';
     }, { once: true });
 
-    userMenu.style.animation = "MoveOutScale 0.3s";
+    userMenu.style.animation = "MoveOutScale 0.4s";
+
+    if (screenWidth <= 768) { // Điều kiện cho màn hình điện thoại (thay đổi số liệu tùy theo yêu cầu)
+        userMenu.addEventListener('animationend', function() {
+            userMenu.style.display = "none";
+            userMenu.style.animation = ""; // Reset animation
+            document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
+        }, { once: true });
+
+        userMenu.style.animation = "MoveOutRight 0.6s"; // Áp dụng animation khi kích thước là điện thoại
+    }
 }
+
 
 
 function DisplayLanguageOption() {
