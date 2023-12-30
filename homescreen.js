d33ea0f4d13e32368692b4eaefdae23dff59b71d
NamVuonggg <NamVuong453@.com>
2023-12-31 08:55:42 +0900

    updated 31/12/2023

--- homescreen.js
+++ homescreen.js
@@ -5,32 +5,50 @@ function DisplayUserOption() {
     userMenu.style.display = "block";
 }
 
+// Đặt biến để lưu trữ trạng thái hiện tại của menu (đã mở hay đã đóng)
+var isMenuOpen = false;
+
+// Hàm để ẩn menu
 function HideUserOption() {
     var userMenu = document.getElementById("user-menu-container");
-    var screenWidth = window.innerWidth;
-
     
-    // Thêm event listener để ẩn phần tử sau khi animation hoàn thành
+    // Thêm event listener để ẩn menu sau khi animation hoàn thành
     userMenu.addEventListener('animationend', function() {
         userMenu.style.display = "none";
         userMenu.style.animation = ""; // Reset animation
-        
-        // Khi menu đã ẩn, hiển thị lại thanh cuộn
-        document.querySelector('body').style.overflow = '';
+        document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
+        isMenuOpen = false; // Cập nhật trạng thái menu thành đã đóng
     }, { once: true });
 
-    userMenu.style.animation = "MoveOutScale 0.4s";
+    userMenu.style.animation = "MoveOutRight 0.5s"; // Áp dụng animation để đóng menu
+}
 
-    if (screenWidth <= 768) { // Điều kiện cho màn hình điện thoại (thay đổi số liệu tùy theo yêu cầu)
-        userMenu.addEventListener('animationend', function() {
-            userMenu.style.display = "none";
-            userMenu.style.animation = ""; // Reset animation
-            document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
-        }, { once: true });
+// Hàm để kiểm tra sự kiện nhấp chuột ngoài menu và ẩn menu nếu cần
+function ClickOutsideMenu(event) {
+    var userMenu = document.getElementById("user-menu-container");
+    var targetElement = event.target;
 
-        userMenu.style.animation = "MoveOutRight 0.6s"; // Áp dụng animation khi kích thước là điện thoại
+    if (isMenuOpen && !userMenu.contains(targetElement)) {
+        // Nếu menu đang mở và người dùng nhấp ngoài menu
+        HideUserOption(); // Ẩn menu
     }
 }
+
+// Thêm sự kiện lắng nghe để kiểm tra nhấp chuột ngoài menu
+document.addEventListener('click', ClickOutsideMenu);
+
+
+    // if (screenWidth <= 768) { // Điều kiện cho màn hình điện thoại (thay đổi số liệu tùy theo yêu cầu)
+    //     userMenu.addEventListener('animationend', function() {
+    //         userMenu.style.display = "none";
+    //         userMenu.style.animation = ""; // Reset animation
+    //         document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
+    //     }, { once: true });
+
+    //     userMenu.style.animation = "MoveOutRight 0.6s"; // Áp dụng animation khi kích thước là điện thoại
+    // }
+
+
 
 
 
