0525949f1ec441cecd3383a8d2abc473fea87e64
NamVuonggg <NamVuong453@gmail.com>
2023-12-31 21:04:21 +0900

    updated 31/12/2023

--- homescreen.js
+++ homescreen.js
@@ -5,8 +5,6 @@ function DisplayUserOption() {
     userMenu.style.display = "block";
 }
 
-// Đặt biến để lưu trữ trạng thái hiện tại của menu (đã mở hay đã đóng)
-var isMenuOpen = false;
 
 // Hàm để ẩn menu
 function HideUserOption() {
@@ -17,36 +15,22 @@ function HideUserOption() {
         userMenu.style.display = "none";
         userMenu.style.animation = ""; // Reset animation
         document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
-        isMenuOpen = false; // Cập nhật trạng thái menu thành đã đóng
     }, { once: true });
 
-    userMenu.style.animation = "MoveOutRight 0.5s"; // Áp dụng animation để đóng menu
-}
+    userMenu.style.animation = "MoveOutScale 0.5s"; // Áp dụng animation để đóng menu
 
-// Hàm để kiểm tra sự kiện nhấp chuột ngoài menu và ẩn menu nếu cần
-function ClickOutsideMenu(event) {
-    var userMenu = document.getElementById("user-menu-container");
-    var targetElement = event.target;
+    var screenWidth = window.innerWidth;
 
-    if (isMenuOpen && !userMenu.contains(targetElement)) {
-        // Nếu menu đang mở và người dùng nhấp ngoài menu
-        HideUserOption(); // Ẩn menu
+    if (screenWidth <= 768) { // Điều kiện cho màn hình điện thoại (thay đổi số liệu tùy theo yêu cầu)
+        userMenu.addEventListener('animationend', function() {
+            userMenu.style.display = "none";
+            userMenu.style.animation = ""; // Reset animation
+            document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
+        }, { once: true });
+
+        userMenu.style.animation = "MoveOutRight 0.6s"; // Áp dụng animation khi kích thước là điện thoại
     }
 }
-
-// Thêm sự kiện lắng nghe để kiểm tra nhấp chuột ngoài menu
-document.addEventListener('click', ClickOutsideMenu);
-
-
-    // if (screenWidth <= 768) { // Điều kiện cho màn hình điện thoại (thay đổi số liệu tùy theo yêu cầu)
-    //     userMenu.addEventListener('animationend', function() {
-    //         userMenu.style.display = "none";
-    //         userMenu.style.animation = ""; // Reset animation
-    //         document.querySelector('body').style.overflow = ''; // Hiển thị lại thanh cuộn
-    //     }, { once: true });
-
-    //     userMenu.style.animation = "MoveOutRight 0.6s"; // Áp dụng animation khi kích thước là điện thoại
-    // }
 
 
 
