f44a2feeab1f0a749447d30ee4304c0c17c6928b
NamVuonggg <NamVuong453@gmail.com>
2024-04-10 13:16:41 +0900

    updated 10/4/2024

--- js/homescreen.js
+++ js/homescreen.js
@@ -55,197 +55,251 @@ function DisplayLanguageOption() {
 
 
 
-// START Phan button va dots cua advertisement 
-let list = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let dots = document.querySelectorAll('.slider .dots li');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
+// // START Phan button va dots cua advertisement 
+// let list = document.querySelector('.slider .list');
+// let items = document.querySelectorAll('.slider .list .item');
+// let dots = document.querySelectorAll('.slider .dots li');
+// let prev = document.getElementById('prev');
+// let next = document.getElementById('next');
 
-let active = 0;
-let lengItems = items.length - 1;
+// let active = 0;
+// let lengItems = items.length - 1;
 
-next.onclick = function (){
-    if(active + 1 > lengItems){
-        active = 0;
-    }else{
-        active = active + 1;
-    }
-    reloadSlider();
-}
+// next.onclick = function (){
+//     if(active + 1 > lengItems){
+//         active = 0;
+//     }else{
+//         active = active + 1;
+//     }
+//     reloadSlider();
+// }
 
-prev.onclick = function(){
-    if(active -1 < 0){
-        active = lengItems;
-    }else{
-        active = active - 1;
-    }
-    reloadSlider();
-}
+// prev.onclick = function(){
+//     if(active -1 < 0){
+//         active = lengItems;
+//     }else{
+//         active = active - 1;
+//     }
+//     reloadSlider();
+// }
 
-let refreshSlider = setInterval(()=>{next.click()},5000);
+// let refreshSlider = setInterval(()=>{next.click()},5000);
 
-function reloadSlider (){
-    let checkLeft = items[active].offsetLeft;
-    list.style.left = -checkLeft + 'px';
+// function reloadSlider (){
+//     let checkLeft = items[active].offsetLeft;
+//     list.style.left = -checkLeft + 'px';
 
-    let lastActiveDot = document.querySelector('.slider .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
-    clearInterval(refreshSlider);
-    refreshSlider = setInterval(()=> {next.click()},5000)
-} 
+//     let lastActiveDot = document.querySelector('.slider .dots li.active');
+//     lastActiveDot.classList.remove('active');
+//     dots[active].classList.add('active');
+//     clearInterval(refreshSlider);
+//     refreshSlider = setInterval(()=> {next.click()},5000)
+// } 
   
-dots.forEach((li, key) => {
-    li.addEventListener('click', function() {
-        active = key;
-        reloadSlider();
-    });
-});
+// dots.forEach((li, key) => {
+//     li.addEventListener('click', function() {
+//         active = key;
+//         reloadSlider();
+//     });
+// });
 
-// Hàm xử lý thanh cuộn category (KHONG SU DUNG NUA)
+// // Hàm xử lý thanh cuộn category (KHONG SU DUNG NUA)
 
-const tabs = document.querySelectorAll(".category-slider ul li a");
-const rightArrow = document.querySelector(".category-slider .right-arrow svg");
-const leftArrow = document.querySelector(".category-slider .left-arrow svg");
-const tabsList = document.querySelector(".category-slider ul");
-const leftArrowContainer = document.querySelector(".category-slider .left-arrow");
-const rightArrowContainer = document.querySelector(".category-slider .right-arrow");
+// const tabs = document.querySelectorAll(".category-slider ul li a");
+// const rightArrow = document.querySelector(".category-slider .right-arrow svg");
+// const leftArrow = document.querySelector(".category-slider .left-arrow svg");
+// const tabsList = document.querySelector(".category-slider ul");
+// const leftArrowContainer = document.querySelector(".category-slider .left-arrow");
+// const rightArrowContainer = document.querySelector(".category-slider .right-arrow");
 
 
-const removeAllActiveClasses = () => {
-    tabs.forEach(tab => {
-        tab.classList.remove("category-active");
-    })
-}
+// const removeAllActiveClasses = () => {
+//     tabs.forEach(tab => {
+//         tab.classList.remove("category-active");
+//     })
+// }
 
-tabs.forEach(tab => {
-    tab.addEventListener("click",() => {
-        removeAllActiveClasses();
-        tab.classList.add("category-active");
-    })
-});
+// tabs.forEach(tab => {
+//     tab.addEventListener("click",() => {
+//         removeAllActiveClasses();
+//         tab.classList.add("category-active");
+//     })
+// });
 
-const manageIcons = () => {
-    if (tabsList.scrollLeft >= 20){
-        leftArrowContainer.classList.add("category-active");
-    } else{
-        leftArrowContainer.classList.remove("category-active");
+// const manageIcons = () => {
+//     if (tabsList.scrollLeft >= 20){
+//         leftArrowContainer.classList.add("category-active");
+//     } else{
+//         leftArrowContainer.classList.remove("category-active");
+//     }
+
+//     let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
+
+//     if(tabsList.scrollLeft >= maxScrollValue) {
+//         rightArrowContainer.classList.remove("category-active");
+//     } else {
+//         rightArrowContainer.classList.add("category-active")
+//     }
+// }
+
+// const shiftValue = window.innerWidth < 1025 ? 760 : 650; 
+// // < 1025px, 760: ipad, 650: PC 
+// rightArrow.addEventListener("click",() => {
+//     tabsList.scrollLeft += shiftValue;
+//     manageIcons();
+// });
+
+// leftArrow.addEventListener("click",() => {
+//     tabsList.scrollLeft -= shiftValue;
+//     manageIcons();
+// });
+
+// tabsList.addEventListener("scroll", manageIcons);
+
+
+
+// // Hàm xử lý thanh cuộn store (KHONG SU DUNG NUA)
+
+// const tabs2 = document.querySelectorAll(".store-slider ul li a");
+// const rightArrow2 = document.querySelector(".store-right-arrow svg");
+// const leftArrow2 = document.querySelector(".store-left-arrow svg");
+// const tabsList2 = document.querySelector(".store-slider ul");
+// const leftArrowContainer2 = document.querySelector(".store-left-arrow");
+// const rightArrowContainer2 = document.querySelector(".store-right-arrow");
+
+
+// const removeAllActiveClasses2 = () => {
+//     tabs2.forEach(tab => {
+//         tab.classList.remove("store-active");
+//     })
+// }
+
+// tabs2.forEach(tab => {
+//     tab.addEventListener("click",() => {
+//         removeAllActiveClasses();
+//         tab.classList.add("store-active");
+//     })
+// });
+
+// const manageIcons2 = () => {
+//     if (tabsList2.scrollLeft >=20){
+//         leftArrowContainer2.classList.add("store-active");
+//     } else{
+//         leftArrowContainer2.classList.remove("store-active");
+//     }
+
+//     let maxScrollValue = tabsList2.scrollWidth - tabsList2.clientWidth -20;
+//     console.log("scroll width: ", tabsList2.scrollWidth);
+//     console.log("client width: ", tabsList2.clientWidth);
+
+// }
+
+// rightArrow2.addEventListener("click",() => {
+//     tabsList2.scrollLeft += 600;
+//     manageIcons()
+// })
+
+// leftArrow2.addEventListener("click",() => {
+//     tabsList2.scrollLeft -= 600;
+//     manageIcons()
+// })
+
+// tabsList2.addEventListener("scroll", manageIcons);
+
+
+
+
+
+// // Hàm xử lý sự kiện vuốt trên điện thoại (KHONG SU DUNG NUA)
+// function handleTouchDrag() {
+//     let isDragging = false;
+//     let startX, endX;
+//     let initialLeft;
+
+//     list.addEventListener('touchstart', (e) => {
+//         isDragging = true;
+//         startX = e.touches[0].pageX - list.offsetLeft;
+//         initialLeft = list.offsetLeft;
+//     });
+
+//     list.addEventListener('touchmove', (e) => {
+//         if (!isDragging) return;
+//         e.preventDefault();
+
+//         endX = e.touches[0].pageX - list.offsetLeft;
+//         const move = endX - startX;
+//         list.style.left = `${initialLeft + move}px`;
+//     });
+
+//     list.addEventListener('touchend', () => {
+//         isDragging = false;
+//         const move = endX - startX;
+//         if (move < -50) {
+//             next.click();
+//         } else if (move > 50) {
+//             prev.click();
+//         } else {
+//             reloadSlider();
+//         }
+//     });
+// }
+
+// // Sử dụng hàm handleTouchDrag() cho sự kiện vuốt trên điện thoại
+// handleTouchDrag();
+
+// WEATHER PART
+
+let valueSearch = document.getElementById('valueSearch');
+let city = document.getElementById('city');
+let temperature = document.getElementById('temperature');
+let description = document.querySelector('.description');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
+let form = document.querySelector('form');
+let main = document.querySelector('main-wth-widget');
+
+form.addEventListener('submit', (event) => {
+ event.preventDefault(); // Ngăn chặn hành vi mặc định của biểu mẫu
+    if (valueSearch.value !== '') {
+        searchWeather();
     }
-
-    let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
-
-    if(tabsList.scrollLeft >= maxScrollValue) {
-        rightArrowContainer.classList.remove("category-active");
-    } else {
-        rightArrowContainer.classList.add("category-active")
-    }
-}
-
-const shiftValue = window.innerWidth < 1025 ? 760 : 650; 
-// < 1025px, 760: ipad, 650: PC 
-rightArrow.addEventListener("click",() => {
-    tabsList.scrollLeft += shiftValue;
-    manageIcons();
-});
-
-leftArrow.addEventListener("click",() => {
-    tabsList.scrollLeft -= shiftValue;
-    manageIcons();
-});
-
-tabsList.addEventListener("scroll", manageIcons);
-
-
-
-// Hàm xử lý thanh cuộn store (KHONG SU DUNG NUA)
-
-const tabs2 = document.querySelectorAll(".store-slider ul li a");
-const rightArrow2 = document.querySelector(".store-right-arrow svg");
-const leftArrow2 = document.querySelector(".store-left-arrow svg");
-const tabsList2 = document.querySelector(".store-slider ul");
-const leftArrowContainer2 = document.querySelector(".store-left-arrow");
-const rightArrowContainer2 = document.querySelector(".store-right-arrow");
-
-
-const removeAllActiveClasses2 = () => {
-    tabs2.forEach(tab => {
-        tab.classList.remove("store-active");
-    })
-}
-
-tabs2.forEach(tab => {
-    tab.addEventListener("click",() => {
-        removeAllActiveClasses();
-        tab.classList.add("store-active");
-    })
-});
-
-const manageIcons2 = () => {
-    if (tabsList2.scrollLeft >=20){
-        leftArrowContainer2.classList.add("store-active");
-    } else{
-        leftArrowContainer2.classList.remove("store-active");
-    }
-
-    let maxScrollValue = tabsList2.scrollWidth - tabsList2.clientWidth -20;
-    console.log("scroll width: ", tabsList2.scrollWidth);
-    console.log("client width: ", tabsList2.clientWidth);
-
-}
-
-rightArrow2.addEventListener("click",() => {
-    tabsList2.scrollLeft += 600;
-    manageIcons()
 })
 
-leftArrow2.addEventListener("click",() => {
-    tabsList2.scrollLeft -= 600;
-    manageIcons()
-})
+let id = '9505fd1df737e20152fbd78cdb289b6a'; // Thay YOUR_API_KEY bằng API key của bạn
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
 
-tabsList2.addEventListener("scroll", manageIcons);
+const searchWeather = () => {
+    fetch(url + '&q=' + valueSearch.value)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.cod == 200) {
+                city.querySelector("figcaption").innerText = data.name;
+                city.querySelector('img').src = 'https://flagsapi.com/'+data.sys.country+'/shiny/32.png';
 
+                temperature.querySelector('img').src='http://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png';
+                temperature.querySelector('figcaption span').innerText = data.main.temp;
+                description.innerText = data.weather[0].description;
+                clouds.innerText = data.clouds.all;
+                humidity.innerText = data.main.humidity;
+                pressure.innerText = data.main.pressure;
+            }else{
+                alert('Your search may have some problems or name of the city does not exist');
+                setTimeout(() => {
+                    main.classList.remove('error');
+                },1000);
+            }
 
-
-
-
-// Hàm xử lý sự kiện vuốt trên điện thoại (KHONG SU DUNG NUA)
-function handleTouchDrag() {
-    let isDragging = false;
-    let startX, endX;
-    let initialLeft;
-
-    list.addEventListener('touchstart', (e) => {
-        isDragging = true;
-        startX = e.touches[0].pageX - list.offsetLeft;
-        initialLeft = list.offsetLeft;
-    });
-
-    list.addEventListener('touchmove', (e) => {
-        if (!isDragging) return;
-        e.preventDefault();
-
-        endX = e.touches[0].pageX - list.offsetLeft;
-        const move = endX - startX;
-        list.style.left = `${initialLeft + move}px`;
-    });
-
-    list.addEventListener('touchend', () => {
-        isDragging = false;
-        const move = endX - startX;
-        if (move < -50) {
-            next.click();
-        } else if (move > 50) {
-            prev.click();
-        } else {
-            reloadSlider();
-        }
-    });
+        })
+}
+const initApp = () => {
+    valueSearch.value = 'Saitama';
+    searchWeather();
 }
 
-// Sử dụng hàm handleTouchDrag() cho sự kiện vuốt trên điện thoại
-handleTouchDrag();
+initApp();
+
 
 
 
